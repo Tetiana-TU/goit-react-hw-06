bf4a28a4f64429ef138a2e481f5ed5d7df3b241b
Tetiana Dmytryienko <dmitrienko_tanya@ukr.net>
2025-03-26 09:02:10 +0200

    hw6reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,35 +3,25 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // використовуємо localStorage
 import contactsReducer from "./contactsSlice";
 import filtersReducer from "./filtersSlice";
-import { combineReducers } from "redux"; // імпортуємо combineReducers
 
-// Конфігурація для persist
 const persistConfig = {
   key: "contacts",
   storage,
-  whitelist: ["items"], // зберігаємо тільки масив контактів
+  whitelist: ["items"],
 };
 
-// Створюємо persistedContactsReducer за допомогою persistReducer
 const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
 
-// Комбінуємо ред'юсери
-const rootReducer = combineReducers({
-  contacts: persistedContactsReducer,
-  filters: filtersReducer,
-});
-
-// Створюємо store
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: { contacts: persistedContactsReducer, filters: filtersReducer },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: ["persist/PERSIST"],
-      }, // Використовуємо rootReducer
+      },
     }),
 });
-// Створюємо persistor для використання з PersistGate
+
 const persistor = persistStore(store);
 
 export { store, persistor };
